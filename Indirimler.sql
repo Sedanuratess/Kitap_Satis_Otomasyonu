b8be96281691dd8afb3e2364814ead977ac7d667
Ünzile Şule Dalkılıç <128928413+unziledalkilic@users.noreply.github.com>
2024-11-04 21:20:21 +0300

    Delete Indirimler.sql

--- Indirimler.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Indirimler (
-    IndirimID INT PRIMARY KEY IDENTITY(1,1),
-    MusteriID INT,
-    SatisID INT,
-    IndirimTutari DECIMAL(10, 2),
-    IndirimTarihi DATETIME NOT NULL
-
-);
